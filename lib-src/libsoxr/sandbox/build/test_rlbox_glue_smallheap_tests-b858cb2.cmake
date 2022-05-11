943a20c846c1a32ce03db1d6ca722edc847ebe40
Christopher Carrasco <chris.carrasco@att.net>
2022-05-10 21:29:14 -0500

    removed build folder

--- lib-src/libsoxr/sandbox/build/test_rlbox_glue_smallheap_tests-b858cb2.cmake
+++
@@ -1,4 +0,0 @@
-add_test( [==[sandbox glue tests rlbox_wasm2c_sandbox smallheap]==] [==[/Users/topher/Documents/Spring2022/cs361s/final project/rlbox_wasm2c_sandbox/build/test_rlbox_glue_smallheap]==] [==[sandbox glue tests rlbox_wasm2c_sandbox smallheap]==])
-set_tests_properties( [==[sandbox glue tests rlbox_wasm2c_sandbox smallheap]==] PROPERTIES WORKING_DIRECTORY [==[/Users/topher/Documents/Spring2022/cs361s/final project/rlbox_wasm2c_sandbox/build]==])
-set_tests_properties( [==[sandbox glue tests rlbox_wasm2c_sandbox smallheap]==] PROPERTIES LABELS [==[[sandbox_glue_tests]]==])
-set( test_rlbox_glue_smallheap_TESTS [==[sandbox glue tests rlbox_wasm2c_sandbox smallheap]==])
--- lib-src/libsoxr/sandbox/build/test_rlbox_glue_static_tests-b858cb2.cmake
+++
@@ -1,4 +0,0 @@
-add_test( [==[sandbox glue tests rlbox_wasm2c_sandbox static]==] [==[/Users/topher/Documents/Spring2022/cs361s/final project/rlbox_wasm2c_sandbox/build/test_rlbox_glue_static]==] [==[sandbox glue tests rlbox_wasm2c_sandbox static]==])
-set_tests_properties( [==[sandbox glue tests rlbox_wasm2c_sandbox static]==] PROPERTIES WORKING_DIRECTORY [==[/Users/topher/Documents/Spring2022/cs361s/final project/rlbox_wasm2c_sandbox/build]==])
-set_tests_properties( [==[sandbox glue tests rlbox_wasm2c_sandbox static]==] PROPERTIES LABELS [==[[sandbox_glue_tests]]==])
-set( test_rlbox_glue_static_TESTS [==[sandbox glue tests rlbox_wasm2c_sandbox static]==])
